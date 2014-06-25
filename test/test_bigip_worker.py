6456c0383284f16041d286e2a22375a35a7d75b9
Tim Bielawa <tbielawa@redhat.com>
2014-06-25 13:04:45 -0400

    Actual test commands here now [ci skip]

---
+++ test/test_bigip_worker.py
@@ -0,0 +1,138 @@
+# Copyright (C) 2014 SEE AUTHORS FILE
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Unittests for the BigIP Worker
+"""
+
+import pika
+import mock
+import copy
+
+from . import TestCase
+from replugin import bigipworker
+
+MQ_CONF = {
+    'server': '127.0.0.1',
+    'port': 5672,
+    'vhost': '/',
+    'user': 'guest',
+    'password': 'guest',
+}
+
+_base_params = {
+    'dynamic': {},
+    'group': 'test',
+    'notify': {},
+    'parameters': {
+        'command': 'bigip'
+    }
+}
+
+class TestBigipWorker(TestCase):
+
+    def setUp(self):
+        ##############################################################
+        # Parameters dict where the subcommand is invalid
+        self.subcommand_params_bad = copy.deepcopy(_base_params)
+        self.subcommand_params_bad['parameters'].update({
+            'subcommand': 'invalidsubcommand'
+        })
+
+        ##############################################################
+        # Create a valid in/outofrotation parameters dict
+        self.rotation_params_good = copy.deepcopy(_base_params)
+        # Rotation commands require a list of hosts
+        self.rotation_params_good['parameters'].update({
+            'hosts': ['localhost'],
+            'subcommand': 'InRotation'
+        })
+
+        ##############################################################
+        # Create an invalid in/outofrotation parameters dict
+        self.rotation_params_bad = copy.deepcopy(_base_params)
+        # Rotation command without the required 'hosts' parameter
+        self.rotation_params_bad['parameters'].update({
+            'subcommand': 'InRotation'
+        })
+
+        ##############################################################
+        # Create a valid configsync parameters dict
+        self.configsync_params_good = copy.deepcopy(_base_params)
+        # Sync command with the required environments (envs)
+        self.configsync_params_good['parameters'].update({
+            'subcommand': 'Configsync',
+            'envs': ['qa']
+        })
+
+        ##############################################################
+        # Create an invalid configsync parameters dict
+        self.configsync_params_bad = copy.deepcopy(_base_params)
+        # Sync command WITHOUT the required environments (envs)
+        self.configsync_params_bad['parameters'].update({
+            'subcommand': 'ConfigSync'
+        })
+
+        ##############################################################
+        # Other random stuff
+        self.channel = mock.MagicMock('pika.spec.Channel')
+        self.channel.basic_consume = mock.Mock('basic_consume')
+        self.channel.basic_ack = mock.Mock('basic_ack')
+        self.channel.basic_publish = mock.Mock('basic_publish')
+
+        self.basic_deliver = mock.MagicMock()
+        self.basic_deliver.delivery_tag = 123
+
+        self.properties = mock.MagicMock(
+            'pika.spec.BasicProperties',
+            correlation_id=123,
+            reply_to='me')
+
+        self.app_logger = mock.MagicMock('logging.Logger').__call__()
+        self.logger = mock.MagicMock('logging.Logger').__call__()
+        self.connection = mock.MagicMock('pika.SelectConnection')
+
+    @mock.patch('pika.SelectConnection')
+    def test_subcommand_invalid(self):
+        """Invalid subcommands are caught and abort"""
+        # self.subcommand_params_bad
+        worker = bigipworker.BigipWorker(
+            MQ_CONF,
+            logger=self.app_logger,
+            output_dir='/tmp/logs/')
+
+        worker._on_open(self.connection)
+        worker._on_channel_open(self.channel)
+
+
+
+    def test_configsync_validation_good(self):
+        """Good configsync params pass validation"""
+        # self.configsync_params_good
+        pass
+
+    def test_configsync_validation_bad(self):
+        """Bad configsync params are caught and abort"""
+        # self.configsync_params_bad
+        pass
+
+    def test_rotation_validation_good(self):
+        """Good rotation params pass validation"""
+        # self.rotation_params_good
+        pass
+
+    def test_rotation_validation_bad(self):
+        """Bad rotation params are caught and abort"""
+        # self.rotation_params_bad
+        pass
